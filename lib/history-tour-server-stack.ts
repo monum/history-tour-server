20bb1e3cef434b65fdbd166bb5ffa485c34cfe4c
sagarrat7 <sagarrat7@gmail.com>
2023-07-10 20:16:16 -0700

    add s3 buckets to store tour assets (images and audio files), add dynamodb tables to store tour metadata

--- lib/history-tour-server-stack.ts
+++ lib/history-tour-server-stack.ts
@@ -1,19 +1,33 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
+import {aws_s3 as s3} from 'aws-cdk-lib';
+import { aws_dynamodb as dynamodb } from 'aws-cdk-lib';
 
 export class HistoryTourServerStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'HistoryTourServerQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    new s3.Bucket(this, 'TourAssetsBucket', {
+      versioned: true,
+
+    });
+    
+    new s3.Bucket(this, 'UnapprovedStoryAssetsBucket', {
+      versioned: true,
+
     });
 
-    const topic = new sns.Topic(this, 'HistoryTourServerTopic');
+    new dynamodb.Table(this, 'RouteTable', {
+      partitionKey: { name: 'routeName', type: dynamodb.AttributeType.STRING }
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    new dynamodb.Table(this, 'ApprovedUserStoryTable', {
+      partitionKey: { name: 'uniqueIdentifier', type: dynamodb.AttributeType.STRING }
+    });
+
+    new dynamodb.Table(this, 'UnapprovedUserStoryTable', {
+      partitionKey: { name: 'uniqueIdentifier', type: dynamodb.AttributeType.STRING }
+    });
+    
   }
 }
