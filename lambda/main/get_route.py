3625d250b14317b80a7794948c440dc3f4bf6682
sagarrat7 <sagarrat7@gmail.com>
2023-07-11 22:14:16 -0700

    get route lambda function

--- lambda/main/get_route.py
+++ lambda/main/get_route.py
@@ -1,1 +1,45 @@
 import json
+import boto3
+from boto3.dynamodb.types import TypeDeserializer
+
+dynamodb = boto3.client('dynamodb')
+deserializer = TypeDeserializer()
+
+
+def handler(event, context):
+    routeName = ""
+    responseCode = 200
+
+    print("request: " + json.dumps(event))
+
+    #todo - mapping from S3 buckets for images and audio
+
+    if (event["queryStringParameters"] and event["queryStringParameters"]["routeName"]):
+        print("Received query: " + event["queryStringParameters"]["routeName"])
+        routeName = event["queryStringParameters"]["routeName"]
+
+        # todo - update TableName
+        db_response = dynamodb.get_item(
+            TableName = "HistoryTourServerAppStack-RouteTableTest64965DD4-1FJF389OM46QH", 
+            Key={
+                'routeName': {'S': routeName}
+            }
+        )
+
+        document = db_response['Item']
+        #print(document)
+        deserialized_document = {k: deserializer.deserialize(v) for k, v in document.items()}
+        response_body = deserialized_document
+    else:
+        message = "Please give valid parameter"
+        response_body = {"message": message, "input": event}
+
+    response = {
+            "statusCode": responseCode,
+            "headers": {
+                "Content-Type" : "application/json"
+            },
+            "body": json.dumps(response_body)
+        }
+    return response
+
--- lib/history-tour-server-stack.ts
+++ lib/history-tour-server-stack.ts
@@ -3,6 +3,8 @@ import { Construct } from 'constructs';
 import {aws_s3 as s3} from 'aws-cdk-lib';
 import { aws_dynamodb as dynamodb } from 'aws-cdk-lib';
 import { aws_apigateway as apigateway } from 'aws-cdk-lib';
+import { aws_lambda as lambda } from 'aws-cdk-lib'; 
+
 
 export class HistoryTourServerStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -20,7 +22,7 @@ export class HistoryTourServerStack extends Stack {
     });
 
     // create dynamodb tables
-    new dynamodb.Table(this, 'RouteTable', {
+    const routeTable = new dynamodb.Table(this, 'RouteTable', {
       partitionKey: { name: 'routeName', type: dynamodb.AttributeType.STRING }
     });
 
@@ -39,8 +41,15 @@ export class HistoryTourServerStack extends Stack {
         allowOrigins: apigateway.Cors.ALL_ORIGINS } //TODO:update
     });
 
-    //
+    // create get_route lambda function
+    const getroute = new lambda.Function(this, 'GetRouteHandler', {
+      runtime: lambda.Runtime.PYTHON_3_7,    // execution environment
+      code: lambda.Code.fromAsset('lambda/main'),
+      handler: 'get_route.handler'                
+    });
+    // grant dynamodb read permissions
+    routeTable.grantReadData(getroute);
 
-
+    //TODO - add rest of lambda functions
   }
 }
