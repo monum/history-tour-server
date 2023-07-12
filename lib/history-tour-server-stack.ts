e993de49edca5695446b9dfdd1f3f60cb752de2b
sagarrat7 <sagarrat7@gmail.com>
2023-07-11 21:26:15 -0700

    add endpoint with api gateway

--- lib/history-tour-server-stack.ts
+++ lib/history-tour-server-stack.ts
@@ -2,11 +2,13 @@ import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import {aws_s3 as s3} from 'aws-cdk-lib';
 import { aws_dynamodb as dynamodb } from 'aws-cdk-lib';
+import { aws_apigateway as apigateway } from 'aws-cdk-lib';
 
 export class HistoryTourServerStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    //create s3 buckets
     new s3.Bucket(this, 'TourAssetsBucket', {
       versioned: true,
 
@@ -17,6 +19,7 @@ export class HistoryTourServerStack extends Stack {
 
     });
 
+    // create dynamodb tables
     new dynamodb.Table(this, 'RouteTable', {
       partitionKey: { name: 'routeName', type: dynamodb.AttributeType.STRING }
     });
@@ -28,6 +31,16 @@ export class HistoryTourServerStack extends Stack {
     new dynamodb.Table(this, 'UnapprovedUserStoryTable', {
       partitionKey: { name: 'uniqueIdentifier', type: dynamodb.AttributeType.STRING }
     });
-    
+
+    // create endpoint with api gateway
+    const api = new apigateway.RestApi(this, 'history-tour-main-api', {
+      description: 'API endpoint for history-tour main application.',
+      defaultCorsPreflightOptions: { 
+        allowOrigins: apigateway.Cors.ALL_ORIGINS } //TODO:update
+    });
+
+    //
+
+
   }
 }
